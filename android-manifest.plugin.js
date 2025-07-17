74c9edeb41c678386bfcc6e86941cedc28f3ef3a
enu-kuro <enu.kuro@gmail.com>
2025-07-17 14:11:24 +0900

    fix

--- android-manifest.plugin.js
+++ android-manifest.plugin.js
@@ -4,11 +4,12 @@ const withForegroundService = (config) => {
   return withAndroidManifest(config, async (config) => {
     const manifest = config.modResults;
 
-    if (!manifest.manifest.$["xmlns:tools"]) {
-      manifest.manifest.$["xmlns:tools"] = "http://schemas.android.com/tools";
-    }
+     if (!manifest.manifest.$["xmlns:tools"]) {
+       manifest.manifest.$["xmlns:tools"] = "http://schemas.android.com/tools";
+     }
 
-    const mainApplication = AndroidConfig.Manifest.getMainApplicationOrThrow(manifest);
+    const mainApplication =
+      AndroidConfig.Manifest.getMainApplicationOrThrow(manifest);
 
     mainApplication["service"] = mainApplication["service"] || [];
     mainApplication["service"].push({
--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,6 @@
-const withForegroundService = require('./android-manifest.plugin');
-import appJson from './app.json';
+import withForegroundService from "./android-manifest.plugin";
+import appJson from "./app.json";
 
 export default withForegroundService({
   ...appJson.expo,
-  android: {
-    ...(appJson.expo.android as any),
-    permissions: [
-      'WAKE_LOCK',
-      'RECORD_AUDIO',
-      'FOREGROUND_SERVICE_MICROPHONE',
-      ...((appJson.expo.android as any).permissions || []),
-    ],
-  },
 });
