d2ba95b8c7fa851e975fe852dc6c60107acdd0d4
leq6c <leq6c6571@outlook.jp>
2025-08-02 14:38:15 +0900

    vapps

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,6 +45,13 @@ export default function TabLayout() {
         }} 
       />
       <Tabs.Screen 
+        name="vapps" 
+        options={{ 
+          title: "vApps", 
+          tabBarIcon: ({ color, size }) => <Ionicons name="apps" size={size} color={color} /> 
+        }} 
+      />
+      <Tabs.Screen 
         name="profile" 
         options={{ 
           title: "Profile", 
